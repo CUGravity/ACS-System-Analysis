382e818860d011756425eb1d5dab19ddfc89267f
sk2873 <sk2873@CIT-PHILLIPS-46.cornell.edu>
2016-11-03 18:41:52 -0400

    Plotter updated with easier to view plots with better plot titles and labels.

--- coilcalcsplotter.m
+++ coilcalcsplotter.m
@@ -64,18 +64,20 @@ for omegaPC = 1:10;
         figure
         subplot(2,1,1);
         hold on;
-        plot3(acW,prcntCW,powerW,'-o');
+        plot3(acW,prcntCW,powerW,'o');
         xlabel('G'); ylabel('Prcnt C'); zlabel('TOTAL Power');
-        title(['OMEGA=' num2str(omegaPC) ' MIN MASS PARAMS']);
-        plot3(MMP(1),MMP(8),MMP(14)+2*MMP(13),'*');
+        title(['OMEGA=' num2str(omegaPC) ' MIN MASS PARAMS, P = f(PC,ac)']);
+        MP = plot3(MMP(1),MMP(8),MMP(14)+2*MMP(13),'*');
+        legend(MP,'Min mass pt');
         hold off;
         
         subplot(2,1,2);
         hold on;
-        plot3(acW,prcntCW,massW,'-o');
+        plot3(acW,prcntCW,massW,'o');
         xlabel('G'); ylabel('Prcnt C'); zlabel('TOTAL Mass');
-        title(['OMEGA=' num2str(omegaPC) ' MIN MASS PARAMS']);
-        plot3(MMP(1),MMP(8),MMP(17),'*');
+        title(['OMEGA=' num2str(omegaPC) ' MIN MASS PARAMS, M = f(PC,ac)']);
+        MP = plot3(MMP(1),MMP(8),MMP(17),'*');
+        legend(MP,'Min mass pt');
         hold off;
     end
 end
@@ -100,18 +102,20 @@ for ipc = 1:5;
         figure
         subplot(2,1,1);
         hold on;
-        plot3(acPC,omegaPC,powerPC,'-o');
+        plot3(acPC,omegaPC,powerPC,'o');
         xlabel('G'); ylabel('Omega'); zlabel('TOTAL Power');
-        title(['PRCNT C=' num2str(prcntC) ' MIN MASS PARAMS']);
-        plot3(MMP(1),MMP(5),MMP(14)+2*MMP(13),'*');
+        title(['PRCNT C=' num2str(prcntC) ' MIN MASS PARAMS, P = f(w,ac)']);
+        MP = plot3(MMP(1),MMP(5),MMP(14)+2*MMP(13),'*');
+        legend(MP,'Min mass pt');
         hold off;
         
         subplot(2,1,2);
         hold on;
-        plot3(acPC,omegaPC,massPC,'-o');
+        plot3(acPC,omegaPC,massPC,'o');
         xlabel('G'); ylabel('Omega'); zlabel('TOTAL Mass');
-        title(['PRCNT C=' num2str(prcntC) ', MIN MASS PARAMS']);
-        plot3(MMP(1),MMP(5),MMP(17),'*');
+        title(['PRCNT C=' num2str(prcntC) ' MIN MASS PARAMS, M = f(w,ac)']);
+        MP = plot3(MMP(1),MMP(5),MMP(17),'*');
+        legend(MP,'Min mass pt');
         hold off;
     end
 end
@@ -137,18 +141,20 @@ for omegaPC = 1:10;
         figure
         subplot(2,1,1);
         hold on;
-        plot3(acW,prcntCW,powerW,'-o');
+        plot3(acW,prcntCW,powerW,'o');
         xlabel('G'); ylabel('Prcnt C'); zlabel('TOTAL Power');
-        title(['OMEGA=' num2str(omegaPC) ' MIN POWER PARAMS']);
-        plot3(MPP(1),MPP(8),MPP(14)+2*MPP(13),'*');
+        title(['OMEGA=' num2str(omegaPC) ' MIN POWER PARAMS, P = f(PC,ac)']);
+        MP = plot3(MPP(1),MPP(8),MPP(14)+2*MPP(13),'*');
+        legend(MP,'Min power pt');
         hold off;
         
         subplot(2,1,2);
         hold on;
-        plot3(acW,prcntCW,massW,'-o');
+        plot3(acW,prcntCW,massW,'o');
         xlabel('G'); ylabel('Prcnt C'); zlabel('TOTAL Mass');
-        title(['OMEGA=' num2str(omegaPC) ' MIN POWER PARAMS']);
-        plot3(MPP(1),MPP(8),MPP(17),'*');
+        title(['OMEGA=' num2str(omegaPC) ' MIN POWER PARAMS, M = f(PC,ac)']);
+        MP = plot3(MPP(1),MPP(8),MPP(17),'*');
+        legend(MP,'Min power pt');
         hold off;
     end
 end
@@ -173,18 +179,20 @@ for ipc = 1:5;
         figure
         subplot(2,1,1);
         hold on;
-        plot3(acPC,omegaPC,powerPC,'-o');
+        plot3(acPC,omegaPC,powerPC,'o');
         xlabel('G'); ylabel('Omega'); zlabel('TOTAL Power');
-        title(['PRCNT C=' num2str(prcntC) ' MIN POWER PARAMS']);
-        plot3(MPP(1),MPP(5),MPP(14)+2*MPP(13),'*');
+        title(['PRCNT C=' num2str(prcntC) ' MIN POWER PARAMS, P = f(w,ac)']);
+        MP = plot3(MPP(1),MPP(5),MPP(14)+2*MPP(13),'*');
+        legend(MP,'Min power pt');
         hold off;
         
         subplot(2,1,2);
         hold on;
-        plot3(acPC,omegaPC,massPC,'-o');
+        plot3(acPC,omegaPC,massPC,'o');
         xlabel('G'); ylabel('Omega'); zlabel('TOTAL Mass');
-        title(['PRCNT C=' num2str(prcntC) ', MIN POWER PARAMS']);
-        plot3(MPP(1),MPP(5),MPP(17),'*');
+        title(['PRCNT C=' num2str(prcntC) ' MIN POWER PARAMS, M = f(w,ac)']);
+        MP = plot3(MPP(1),MPP(5),MPP(17),'*');
+        legend(MP,'Min power pt');
         hold off;
     end
 end
@@ -210,18 +218,20 @@ for omegaPC = 1:10;
         figure
         subplot(2,1,1);
         hold on;
-        plot3(acW,prcntCW,powerW,'-o');
+        plot3(acW,prcntCW,powerW,'o');
         xlabel('G'); ylabel('Prcnt C'); zlabel('TOTAL Power');
-        title(['OMEGA=' num2str(omegaPC) ' MIN CURRENT PARAMS']);
-        plot3(MCurrP(1),MCurrP(8),MCurrP(14)+2*MCurrP(13),'*');
+        title(['OMEGA=' num2str(omegaPC) ' MIN CURRENT PARAMS, P = f(PC,ac)']);
+        MP = plot3(MCurrP(1),MCurrP(8),MCurrP(14)+2*MCurrP(13),'*');
+        legend(MP,'Min current pt');
         hold off;
         
         subplot(2,1,2);
         hold on;
-        plot3(acW,prcntCW,massW,'-o');
+        plot3(acW,prcntCW,massW,'o');
         xlabel('G'); ylabel('Prcnt C'); zlabel('TOTAL Mass');
-        title(['OMEGA=' num2str(omegaPC) ' MIN CURRENT PARAMS']);
-        plot3(MCurrP(1),MCurrP(8),MCurrP(17),'*');
+        title(['OMEGA=' num2str(omegaPC) ' MIN CURRENT PARAMS, M = f(PC,ac)']);
+        MP = plot3(MCurrP(1),MCurrP(8),MCurrP(17),'*');
+        legend(MP,'Min current pt');
         hold off;
     end
 end
@@ -246,18 +256,20 @@ for ipc = 1:5;
         figure
         subplot(2,1,1);
         hold on;
-        plot3(acPC,omegaPC,powerPC,'-o');
+        plot3(acPC,omegaPC,powerPC,'o');
         xlabel('G'); ylabel('Omega'); zlabel('TOTAL Power');
-        title(['PRCNT C=' num2str(prcntC) ' MIN CURRENT PARAMS']);
-        plot3(MCurrP(1),MCurrP(5),MCurrP(14)+2*MCurrP(13),'*');
+        title(['PRCNT C=' num2str(prcntC) ' MIN CURRENT PARAMS, P = f(w,ac)']);
+        MP = plot3(MCurrP(1),MCurrP(5),MCurrP(14)+2*MCurrP(13),'*');
+        legend(MP,'Min current pt');
         hold off;
         
         subplot(2,1,2);
         hold on;
-        plot3(acPC,omegaPC,massPC,'-o');
+        plot3(acPC,omegaPC,massPC,'o');
         xlabel('G'); ylabel('Omega'); zlabel('TOTAL Mass');
-        title(['PRCNT C=' num2str(prcntC) ', MIN CURRENT PARAMS']);
-        plot3(MCurrP(1),MCurrP(5),MCurrP(17),'*');
+        title(['PRCNT C=' num2str(prcntC) ' MIN CURRENT PARAMS, M = f(w,ac)']);
+        MP = plot3(MCurrP(1),MCurrP(5),MCurrP(17),'*');
+        legend(MP,'Min current pt');
         hold off;
     end
 end
@@ -283,18 +295,20 @@ for omegaPC = 1:10;
         figure
         subplot(2,1,1);
         hold on;
-        plot3(acW,prcntCW,powerW,'-o');
+        plot3(acW,prcntCW,powerW,'o');
         xlabel('G'); ylabel('Prcnt C'); zlabel('TOTAL Power');
-        title(['OMEGA=' num2str(omegaPC) ' MIN COST PARAMS']);
-        plot3(MCostP(1),MCostP(8),MCostP(14)+2*MCostP(13),'*');
+        title(['OMEGA=' num2str(omegaPC) ' MIN COST PARAMS, P = f(PC,ac)']);
+        MP = plot3(MCostP(1),MCostP(8),MCostP(14)+2*MCostP(13),'*');
+        legend(MP,'Min cost pt');
         hold off;
         
         subplot(2,1,2);
         hold on;
-        plot3(acW,prcntCW,massW,'-o');
+        plot3(acW,prcntCW,massW,'o');
         xlabel('G'); ylabel('Prcnt C'); zlabel('TOTAL Mass');
-        title(['OMEGA=' num2str(omegaPC) ' MIN COST PARAMS']);
-        plot3(MCostP(1),MCostP(8),MCostP(17),'*');
+        title(['OMEGA=' num2str(omegaPC) ' MIN COST PARAMS, M = f(PC,ac)']);
+        MP = plot3(MCostP(1),MCostP(8),MCostP(17),'*');
+        legend(MP,'Min cost pt');
         hold off;
     end
 end
@@ -319,18 +333,20 @@ for ipc = 1:5;
         figure
         subplot(2,1,1);
         hold on;
-        plot3(acPC,omegaPC,powerPC,'-o');
+        plot3(acPC,omegaPC,powerPC,'o');
         xlabel('G'); ylabel('Omega'); zlabel('TOTAL Power');
-        title(['PRCNT C=' num2str(prcntC) ' MIN COST PARAMS']);
-        plot3(MCostP(1),MCostP(5),MCostP(14)+2*MCostP(13),'*');
+        title(['PRCNT C=' num2str(prcntC) ' MIN COST PARAMS, P = f(w,ac)']);
+        MP = plot3(MCostP(1),MCostP(5),MCostP(14)+2*MCostP(13),'*');
+        legend(MP,'Min cost pt');
         hold off;
         
         subplot(2,1,2);
         hold on;
-        plot3(acPC,omegaPC,massPC,'-o');
+        plot3(acPC,omegaPC,massPC,'o');
         xlabel('G'); ylabel('Omega'); zlabel('TOTAL Mass');
-        title(['PRCNT C=' num2str(prcntC) ', MIN COST PARAMS']);
-        plot3(MCostP(1),MCostP(5),MCostP(17),'*');
+        title(['PRCNT C=' num2str(prcntC) ' MIN COST PARAMS, M = f(w,ac)']);
+        MP = plot3(MCostP(1),MCostP(5),MCostP(17),'*');
+        legend(MP,'Min cost pt');
         hold off;
     end
 end
